8a6ad50097f0f1c5c7aca648e22e92f4509b9a1e
unknown <pyatlosaikumar@gmail.com>
2024-12-03 18:52:34 +0530

    task on operatores1

---
+++ index.js
@@ -0,0 +1,66 @@
+
+if(10 && 10){  
+    alert("hello there good morning")
+}else{
+    alert("hello there see you tmrw !!!!")
+}
+        
+
+if(["sai"] && [1,2,3]){  
+    alert("hello there good morning")
+}else{
+    alert("hello there see you tmrw !!!!")
+}
+
+
+if(["sai"] == [1,2,3]){
+    alert("hello there good morning")
+}else{
+    alert("hello there see you tmrw !!!!")
+}
+
+ 
+
+if(["sai"] && []){  
+    alert("hello there good morning")
+}else{
+    alert("hello there see you tmrw !!!!")
+}
+
+
+
+if(["sai"] && " "){  
+    alert("hello there good morning")
+}else{
+    alert("hello there see you tmrw !!!!")
+}
+
+
+if(["sai"] && undefined){  
+    alert("hello there good morning")
+}else{
+    alert("hello there see you tmrw !!!!")
+}
+
+
+
+if(null && null){  
+    alert("hello there good morning")
+}else{
+    alert("hello there see you tmrw !!!!")
+}
+
+
+if("" || 10){  
+    alert("hello there good morning")
+}else{
+    alert("hello there see you tmrw !!!!")
+}
+
+
+
+if(["sai"] || undefined){  
+    alert("hello there good morning")
+}else{
+    alert("hello there see you tmrw !!!!")
+}
